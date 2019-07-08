b27ebc2a23bc315c4d0c37b1c03f297b1b91b3a9
Josh Hemsley <josh@breathehr.com>
2019-07-08 14:53:15 +0100

    push

---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+
+configure do
+  set :session_secret, "My session secret"
+end
+
+get '/hi' do
+  'Hello!'
+end
+
+get '/secret' do
+  "Yabba Dabba Do!"
+end
+
+get '/forward' do
+  'going somewhere? maybe forward?'
+end
+
+get '/backwards' do
+  'back to the future'
+end
+
+get '/cat' do
+  "<div>
+  <img src='http://bit.ly/1eze8aE'>
+  <div>"
+end
